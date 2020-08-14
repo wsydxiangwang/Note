2f44be5ce0debfeca9cf17030ff5f14bad181fb9
Libai_mac <1915398623@qq.com>
2020-08-15 07:57:00 +0800

    二叉树

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -38,54 +38,128 @@ module.exports = {
 				{
 					title: 'CSS',
 					sidebarDepth: 2,
-					children: ['/web/CSS/1','/web/CSS/2','/web/CSS/3','/web/CSS/4','/web/CSS/5']
+					children: [
+						'/web/CSS/1',
+						'/web/CSS/2',
+						'/web/CSS/3',
+						'/web/CSS/4',
+						'/web/CSS/5'
+					]
 				},
 				{
 					title: 'ES6',
-					children: ['/web/ES6/1','/web/ES6/2','/web/ES6/3']
+					children: [
+						'/web/ES6/1',
+						'/web/ES6/2',
+						'/web/ES6/3'
+					]
 				},
 				{
 					title: 'JS 基础',
 					sidebarDepth: 2,
-					children: ['/web/Core/1','/web/Core/2']
+					children: [
+						'/web/Core/1',
+						'/web/Core/2'
+					]
 				},
 				{
 					title: 'JS 深入数组',
-					children: ['/web/Array/1', '/web/Array/2', '/web/Array/3', '/web/Array/4', '/web/Array/5', '/web/Array/6', '/web/Array/7', '/web/Array/8']
+					children: [
+						'/web/Array/1', 
+						'/web/Array/2', 
+						'/web/Array/3', 
+						'/web/Array/4', 
+						'/web/Array/5', 
+						'/web/Array/6', 
+						'/web/Array/7', 
+						'/web/Array/8'
+					]
 				},
 				{
 					title: 'JS 对象',
-					children: ['/web/Object/1', '/web/Object/2', '/web/Object/3', '/web/Object/4', '/web/Object/5', '/web/Object/6']
+					children: [
+						'/web/Object/1', 
+						'/web/Object/2', 
+						'/web/Object/3', 
+						'/web/Object/4', 
+						'/web/Object/5', 
+						'/web/Object/6'
+					]
 				},
 				{
 					title: 'JS 字符串',
-					children: ['/web/String/1', '/web/String/2', '/web/String/3']
+					children: [
+						'/web/String/1', 
+						'/web/String/2', 
+						'/web/String/3'
+					]
 				},
 				{
 					title: 'this 闭包 原型',
-					children: ['/web/this/1', '/web/this/2', '/web/this/3', '/web/this/4', '/web/this/5']
+					children: [
+						'/web/this/1', 
+						'/web/this/2', 
+						'/web/this/3', 
+						'/web/this/4', 
+						'/web/this/5'
+					]
 				},
 				{
 					title: '异步方法',
-					children: ['/web/Async/1', '/web/Async/2', '/web/Async/3', '/web/Async/4', '/web/Async/5']
+					children: [
+						'/web/Async/1', 
+						'/web/Async/2', 
+						'/web/Async/3', 
+						'/web/Async/4', 
+						'/web/Async/5'
+					]
 				},
 				{
 					title: '性能优化',
 					sidebarDepth: 2,
-					children: ['/web/性能优化/1', '/web/性能优化/2', '/web/性能优化/3', '/web/性能优化/4', '/web/性能优化/5', '/web/性能优化/6', '/web/性能优化/7']
+					children: [
+						'/web/性能优化/1', 
+						'/web/性能优化/2', 
+						'/web/性能优化/3', 
+						'/web/性能优化/4', 
+						'/web/性能优化/5', 
+						'/web/性能优化/6', 
+						'/web/性能优化/7'
+					]
 				},
 				{
 					title: 'http',
 					sidebarDepth: 2,
-					children: ['/web/http/1', '/web/http/2', '/web/http/3', '/web/http/4', '/web/http/5', '/web/http/6', '/web/http/7', '/web/http/8', '/web/http/9', '/web/http/10',]
+					children: [
+						'/web/http/1', 
+						'/web/http/2', 
+						'/web/http/3', 
+						'/web/http/4', 
+						'/web/http/5', 
+						'/web/http/6', 
+						'/web/http/7', 
+						'/web/http/8', 
+						'/web/http/9', 
+						'/web/http/10',
+					]
 				},
 				{
 					title: '稀奇古怪的题',
-					children: ['/web/Other/1', '/web/Other/2', '/web/Other/3', '/web/Other/4', '/web/Other/5', '/web/Other/6']
+					children: [
+						'/web/Other/1', 
+						'/web/Other/2', 
+						'/web/Other/3', 
+						'/web/Other/4', 
+						'/web/Other/5', 
+						'/web/Other/6'
+					]
 				},
 				{
 					title: '正则',
-					children: ['/web/正则/1', '/web/正则/2']
+					children: [
+						'/web/正则/1', 
+						'/web/正则/2'
+					]
 				}
 			],
 			'/after/': [
@@ -116,7 +190,12 @@ module.exports = {
 			'/other/': [
 				{
 					title: '手写代码',
-					children: ['/other/手写/1', '/other/手写/2', '/other/手写/3', '/other/手写/4']
+					children: [
+						'/other/手写/1', 
+						'/other/手写/2', 
+						'/other/手写/3', 
+						'/other/手写/4'
+					]
 				},
 				{
 					title: '设计模式',
@@ -136,6 +215,7 @@ module.exports = {
 						'/other/数据结构/1',
 						'/other/数据结构/2',
 						'/other/数据结构/3',
+						'/other/数据结构/4',
 					]
 				},
 				{
